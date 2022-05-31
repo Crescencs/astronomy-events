30ad96b658bf1b922d6152502ba31476421058c5
Crescencio Perez <crescencio.ps.code@gmail.com>
2022-05-31 05:18:20 -0500

    .

--- JavaScript/datos.js
+++ JavaScript/datos.js
@@ -6,13 +6,11 @@ class Usuario{
         this.contraseña=contraseña;
     }
 }
-const listaUsuarios=[new Usuario("Crescencio123@gmail.com","Crescencio",213477),new Usuario("Paola456@gmail.com","Paola",214445), new Usuario("Maria789@gmail.com","Maria",131062)]; //declarar objeto
-
+const listaUsuarios=[new Usuario("Crescencio123@gmail.com","Crescencio",213477),new Usuario("Paola456@gmail.com","Paola",214445), new Usuario("Maria789@gmail.com","Maria",131062)]; 
 
 localStorage.setItem("usuarios", JSON.stringify(listaUsuarios));
 console.log(JSON.parse(localStorage.getItem("usuarios")));
 
 
 
-//funcion trim(): elimina espacios vacios a una variable
 
